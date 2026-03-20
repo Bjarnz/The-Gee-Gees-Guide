30f0208fee5574e23c9aeaccd1723bab0ebbe778
Benjamin Harris <harrisben458@gmail.com>
2026-03-20 18:29:10 -0400

    Update registration.php

--- backend/registration.php
+++ backend/registration.php
@@ -32,33 +32,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 $mysqli->close();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Register - The Gee-Gees Guide</title>
-</head>
-<body>
-
-<h1>Create Account</h1>
-
-<form method="POST">
-    <label>Username:</label><br>
-    <input type="text" name="username" required><br><br>
-
-    <label>Email:</label><br>
-    <input type="email" name="email" required><br><br>
-
-    <label>Password:</label><br>
-    <input type="password" name="password" required><br><br>
-
-    <input type="submit" value="Register">
-</form>
-   
-<p><?php echo $message; ?></p>
-
-<p>Already have an account? <a href="login.html">Login here</a></p>
-    
-</body>
-</html>
