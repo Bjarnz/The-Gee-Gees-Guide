5af75d8f62d200fee8ebcaa538abec0b44d8b8e6
Benjamin Harris <harrisben458@gmail.com>
2026-03-22 17:25:30 -0400

    Update tutors.php

--- backend/tutors.php
+++ backend/tutors.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['username'])) {
-    header("Location: login.html");
+    header("Location: ../login.html");
     exit();
 }
 // Read the page message and search text from the URL.
